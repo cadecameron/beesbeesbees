82e46e1834e3e3d57854a01e84b53ae5124082d0
cadecameron <cade.cameron@gmail.com>
2020-03-24 15:37:28 -0700

    UPDATED and cleaned-up minor code formatting.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -22,8 +22,10 @@ var ForagerBee = function() {
   this.canFly = true;
   this.treasureChest = [];
 };
+
 ForagerBee.prototype = Object.create(Bee.prototype);
 ForagerBee.prototype.constructor = ForagerBee;
+
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -24,10 +24,11 @@ var HoneyMakerBee = function() {
 
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
 HoneyMakerBee.prototype.makeHoney = function() {
   this.honeyPot++;
 };
+
 HoneyMakerBee.prototype.giveHoney = function() {
   this.honeyPot--;
 };
-
